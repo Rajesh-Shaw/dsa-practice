64da49c830d613af0c4a5f3c182396d144e835c8
Rajesh-Shaw <rkshaw7200@gmail.com>
2025-09-06 20:06:11 +0530

    Update assignment 19, 20

--- C/Switch Case/assignment_19.c
+++ C/Switch Case/assignment_19.c
@@ -31,7 +31,7 @@ Assignment-19: switch case control
 /*
 1. Write a program which takes the month number as an input and display number of days in that month.
 
-*/
+
 #include<stdio.h>
 int main()
 {
@@ -66,7 +66,7 @@ int main()
     return 0;
 }
 
-
+*/
 
 /*
 2. Write a menu driven program with the following options:
@@ -77,12 +77,105 @@ int main()
     5. Exit
     
 
+
+
+#include<stdio.h>
+int main()
+{
+    int choose=0,a=0,b=0;
+    while(1)
+    {
+        printf("\n\n===== MENU =====\n");
+        printf("1. Addition\n");
+        printf("2. Subtraction\n");
+        printf("3. Multiplication\n");
+        printf("4. Division\n");
+        printf("5. Exit\n");
+        printf("=================\n");
+
+        printf("Enter Your Choice : ");
+        scanf("%d",&choose);
+
+        if(choose==5)
+        {
+            printf("Exiting program... Bye Bye :)\n");
+            break;
+        }
+
+        printf("Enter two Number :");
+        scanf("%d%d",&a,&b);
+
+        switch(choose)
+        {
+            case 1: 
+                printf("Result: %d + %d = %d\n", a, b, a+b);
+                break;
+            case 2: 
+                printf("Result: %d - %d = %d\n", a, b, a-b); 
+                break;
+            case 3: 
+                printf("Result: %d * %d = %d\n", a, b, a*b);
+                break;
+            case 4: 
+                if(b != 0)
+                     printf("Result: %d / %d = %.2f\n", a, b, (float)a/b);
+                else
+                    printf("Error! Division by zero is not allowed.\n");
+                break;
+            default: 
+                printf("Invalid choice! Please select between 1 and 5.\n");
+                
+        }
+
+    
+    }
+    return 0;
+}
+
 */
+
+
 
 /*
 3. Write a program which takes the day number of a week and displays a unique greeting message for the day.
-
 */
+
+#include<stdio.h>
+int main()
+{
+    int day;
+    printf("Enter a day Number : ");
+    scanf("%d",&day);
+    switch(day)
+    {
+        case 1:
+            printf("Welcome To Monday");
+            break;
+        case 2:
+            printf("Welcome To Tuesday");
+            break;
+        case 3:
+            printf("Welcome To Wednesday");
+            break;
+        case 4:
+            printf("Welcome To Thursday");
+            break;
+        case 5:
+            printf("Welcome To Friday");
+            break;
+        case 6:
+            printf("Welcome To Saturday");
+            break;
+        case 7:
+            printf("Welcome To Sunday");
+            break;
+        default :
+            printf("Invalid Day Number!");
+    }
+    return 0;
+}
+
+
 
 /*
 4. Write a menu driven program with the following options:
@@ -91,7 +184,69 @@ int main()
     3. Check whether a given set of three numbers are equilateral triangle or not
     4. Exit
 
+
+
+#include<stdio.h>
+int main()
+{
+    int n=0,a=0,b=0,c=0;
+    while(1)
+    {
+        printf("\n\n=========Menu=========\n");
+        printf("1. Check Isosceles triangle\n");
+        printf("2. Check Right angled triangle\n");
+        printf("3. Check Equilateral triangle\n");
+        printf("4. Exit\n");
+        printf("==================\n");
+
+        printf("Enter Your Choice : \n");
+        scanf("%d",&n);
+
+        if(n==4)
+        {
+            printf("Existing Program.............Bye :)");
+            break;
+        }
+        else
+        {
+
+            printf("Enter The all 3 side of triangle : \n");
+            scanf("%d%d%d",&a,&b,&c);
+
+            switch(n)
+            {
+                case 1:
+                    if(a==b || b==c || c==a)
+                        printf("This is an Isosceles triangle");
+                    else
+                        printf("This is not an Isosceles triangle");
+                    break;
+                
+                case 2:
+                    if(a*a + b*b == c*c || b*b + c*c == a*a || c*c + a*a == b*b)
+                        printf("This is a Right angled triangle");
+                    else
+                        printf("This is not a Right angled triangle");
+                    break;
+                
+                case 3:
+                    if(a==b && b==c)
+                        printf("This is an Equilateral triangle");
+                    else
+                        printf("This is not an Equilateral triangle");
+                    break;
+                
+                default :
+                    printf("Please Enter a valid choise.");
+                    
+            }
+        }
+
+
+    }
+}
 */
+
 
 
 /*
@@ -102,5 +257,31 @@ int main()
     else if(var == 2)
     printf("better");
     else if(var == 3)
+
+
+#include<stdio.h>
+int main()
+{
+    int var=0;
+    printf("Enter a Nubmer : ");
+    scanf("%d",&var);
+
+    switch(var)
+    {
+        case 1:
+            printf("Good");
+            break;
+        case 2:
+            printf("Better");
+            break;
+        case 3:
+            printf("Best");
+            break;
+        default :
+            printf("Please Enter number between 1 to 3.");
+
+    }
+}
+
 
 */
--- C/Switch Case/assignment_20.c
+++ C/Switch Case/assignment_20.c
@@ -43,7 +43,7 @@ switch case control
     Marks from 50 to less than 60: Grade E
     Marks below 50: Grade F
     Marks greater than 100 or less than 0: Invalid Marks
-*/
+
 
 #include<stdio.h>
 int main()
@@ -83,7 +83,7 @@ int main()
     return 0;
 }
 
-
+*/
 
 /*
 2. Write a menu driven program with the following options:
@@ -92,19 +92,133 @@ int main()
     3. Area of circle
     4. Sum of first N natural numbers.
     5. Exit
-*/
 
+
+#include<stdio.h>
+int main()
+{
+    int choice=0, n=0;
+    while(1)
+    {
+        printf("Enter Your choice : ");
+        scanf("%d",&choice);
+
+        printf("\n\n==========Menu=========\n");
+        printf("1. Factorial of a number \n");
+        printf("2. Check Even and Odd \n");
+        printf("3. Area of circle \n");
+        printf("4. Sum of first N natural numbers. \n");
+        printf("5. Exit \n");
+        printf("==================\n");
+
+
+
+        if(choice == 5)
+        {
+            printf("Existing Program...........Bye :).");
+            break;
+        }
+        
+        switch(choice)
+        {
+            case 1:
+                int f=1, i=1; 
+                printf("Enter a Number : ");
+                scanf("%d",&n);
+                for(i;i<=n;i++)
+                    f=f*i;
+                printf("Factorial of %d is %d\n",n,f);
+                break;
+            
+            case 2:
+                printf("Enter a Number : ");
+                scanf("%d",&n);
+                if(n%2)
+                    printf("Give Number %d is an Odd number\n",n);
+                else    
+                    printf("Give Number %d is an Even number\n",n);
+                break;
+            
+            case 3:
+                printf("Enter The Radius of Circle : ");
+                scanf("%d",&n);
+                
+                printf("Area of cirle is %0.2f\n",3.14*n*n);
+                break;
+
+            case 4:
+                printf("Enter The Number : ");
+                scanf("%d",&n);
+                
+                printf("Sum of First %d natural numbers = %d\n",n,n*(n+1)/2);
+                break;
+            
+            default :
+                printf("Enter Valid Number.");
+        }
+    }
+    return 0;
+}
+*/
 
 /*
 3. Write a program to check whether a given character is uppercase alphabet or lower case alphabet or some other special character, using switch case statement.
 
+
+
+#include<stdio.h>
+int main()
+{
+    char c;
+    printf("Enter a Character : ");
+    scanf("%c",&c);
+
+    switch(1)
+    {
+        case 1:
+            if(c >='A' && c<= 'Z')
+                printf("The character '%c' is an Uppercase Alphabet.\n", c);
+            else if(c >= 'a' && c <= 'z')
+                printf("The character '%c' is a Lowercase Alphabet.\n", c);
+            else
+                printf("The character '%c' is a Special Character.\n", c);
+            break;
+
+    }
+    return 0;
+}
 */
+
 
 
 /*
 4. Write a program to check whether a given character is vowel or consonant or some other special character, using switch case statement.
 
 */
+
+#include<stdio.h>
+int main()
+{
+    char c;
+    printf("Enter a Character : ");
+    scanf("%c",&c);
+
+    switch(c)
+    {
+        case 'a': case 'e': case 'i': case 'o': case 'u':
+        case 'A': case 'E': case 'I': case 'O': case 'U':
+            printf("The character '%c' is a Vowel.\n", c);
+            break;
+
+        default:
+            if((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+                printf("The character '%c' is a Consonant.\n", c);
+            else
+                printf("The character '%c' is a Special Character.\n", c);
+    }
+
+    return 0;
+}
 
 
 /*
@@ -114,5 +228,113 @@ int main()
     3. Volume of a cuboid
     4. Check whether a given number is Prime or not
     5. Exit
+
+
+
+
+#include<stdio.h>
+int main()
+{
+    int choice=0, n=0;
+    while(1)
+    {
+        printf("Enter Your Choice : ");
+        scanf("%d",&choice);
+
+        printf("\n\n==========Menu=========\n");
+        printf("1. Calculate LCM of two numbers \n");
+        printf("2. Calculate sum of the digits of a number \n");
+        printf("3. Volume of a cuboid \n");
+        printf("4. Check whether a given number is Prime or not. \n");
+        printf("5. Exit \n");
+        printf("==================\n");
+
+
+
+        if(choice == 5)
+        {
+            printf("Existing Program...........Bye :).");
+            break;
+        }
+        
+        switch(choice)
+        {
+            case 1:
+                {
+                    int a=0, b=0, i=0, lcm=0; 
+                    printf("Enter two Number : ");
+                    scanf("%d%d",&a,&b);
+                    if(a>b)
+                        lcm=a;
+                    else
+                        lcm=b;
+
+                    for(i=lcm;i<=a*b;i++)
+                    {
+                        if(i%a==0 && i%b==0)
+                        {
+                            lcm=i;
+                            break;
+                        }
+                    }
+                    printf("LCM of two numbers %d, %d is %d\n",a,b,lcm);
+                    break;
+                }
+            
+            case 2:
+                {
+                    int num=0,sum=0,r=0;
+                    printf("Enter a Number : ");
+                    scanf("%d",&n);
+                    num=n;
+                    while(num)
+                    {
+                        r=num%10;
+                        sum=sum+r;
+                        num=num/10;
+                    }
+                    printf("Sum of the digits of a number %d is %d\n",n,sum);
+                    break;
+                }
+            
+            case 3:
+                {
+                    int a,b,c;
+                    printf("Enter Length, breadth and Height of Cuboid : ");
+                    scanf("%d%d%d",&a,&b,&c);
+                    
+                    printf("Volume of a cuboid is %d\n",a*b*c);
+                    break;
+                }
+
+            case 4:
+                {
+                    int isPrime=1,i=0;
+                    printf("Enter The Number : ");
+                    scanf("%d",&n);
+
+                    for(i=2; i*i<=n; i++)
+                    {
+                        if(n%i==0)
+                        {
+                            isPrime=0;
+                            break;
+                        }
+                    }
+                    
+                    if(isPrime)
+                        printf("Given number %d is a Prime number.\n",n);
+                    else
+                        printf("Given number %d is not a Prime number.\n",n);
+
+                    break;
+                }
+            
+            default :
+                printf("Enter Valid Number.");
+        }
+    }
+    return 0;
+}
 
 */

005667e9e514481428e05016b3dd65c7ffc21f8f
Rajesh-Shaw <rkshaw7200@gmail.com>
2025-09-07 21:47:29 +0530

    Add assignment 21, 22

---
+++ C/Functions/assignment_21.c
@@ -0,0 +1,30 @@
+/*
+C Language
+Assignment-21: Functions
+1. Write a function to calculate the area of a circle. (TSRS)
+2. Write a function to calculate simple interest. (TSRS)
+3. Write a function to check whether a given number is even or odd. Return 1 if the number is even, otherwise return 0. (TSRS)
+4. Write a function to print first N natural numbers (TSRN)
+5. Write a function to print first N odd natural numbers. (TSRN)
+*/
+
+/*
+1. Write a function to calculate the area of a circle. (TSRS)
+*/
+
+/*
+2. Write a function to calculate simple interest. (TSRS)
+*/
+
+/*
+3. Write a function to check whether a given number is even or odd. Return 1 if the number is even, otherwise return 0. (TSRS)
+*/
+
+/*
+4. Write a function to print first N natural numbers (TSRN)
+*/
+
+
+/*
+5. Write a function to print first N odd natural numbers. (TSRN)
+*/

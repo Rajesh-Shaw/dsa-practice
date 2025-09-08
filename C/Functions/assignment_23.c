f2503f28830ec8289f4c9a9a7410c92fedb08119
Rajesh-Shaw <rkshaw7200@gmail.com>
2025-09-08 22:24:40 +0530

    Add assignment 23, 24

---
+++ C/Functions/assignment_23.c
@@ -0,0 +1,30 @@
+/*
+C Language
+Assignment-23: More on Functions
+1. Write a function to calculate LCM of two numbers. (TSRS)
+2. Write a function to calculate HCF of two numbers. (TSRS)
+3. Write a function to check whether a given number is Prime or not. (TSRS)
+4. Write a function to find the next prime number of a given number. (TSRS)
+5. Write a function to print first N prime numbers (TSRN)
+*/
+
+/*
+1. Write a function to calculate LCM of two numbers. (TSRS)
+*/
+
+/*
+2. Write a function to calculate HCF of two numbers. (TSRS)
+*/
+
+/*
+3. Write a function to check whether a given number is Prime or not. (TSRS)
+*/
+
+
+/*
+4. Write a function to find the next prime number of a given number. (TSRS)
+*/
+
+/*
+5. Write a function to print first N prime numbers (TSRN)
+*/
---
+++ C/Functions/assignment_24.c
@@ -0,0 +1,35 @@
+/*
+C Language
+Assignment-24: More on Functions
+1. Write a function to print all Prime numbers between two given numbers. (TSRN)
+2. Write a function to print first N terms of Fibonacci series (TSRN)
+3. Write a function to print PASCAL Triangle. (TSRN)
+4. Write a function to print all Armstrong numbers between two given numbers.
+(TSRN)
+5. Write a function to find the sum of first N terms of the series 11/1+2!/2+3!/3+4!/4+5!/
+5... n!/n (TSRS)
+
+*/
+
+/*
+1. Write a function to print all Prime numbers between two given numbers. (TSRN)
+*/
+
+/*
+2. Write a function to print first N terms of Fibonacci series (TSRN)
+*/
+
+/*
+3. Write a function to print PASCAL Triangle. (TSRN)
+*/
+
+
+/*
+4. Write a function to print all Armstrong numbers between two given numbers.
+(TSRN)
+*/
+
+
+/*
+5. Write a function to find the sum of first N terms of the series 11/1+2!/2+3!/3+4!/4+5!/5... n!/n (TSRS)
+*/

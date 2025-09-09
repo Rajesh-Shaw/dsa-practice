c43ddbdb6a42c90396e9f5b8358437ee761de664
Rajesh-Shaw <rkshaw7200@gmail.com>
2025-09-09 22:14:10 +0530

    Add Assignment 25, 26

---
+++ C/Recursion/assignment_25.c
@@ -0,0 +1,36 @@
+/*
+
+C Language
+Assignment-25: Recursion
+1. Write a recursive function to print first N natural numbers
+2. Write a recursive function to print first N natural numbers in reverse order
+3. Write a recursive function to print first N odd natural numbers
+4. Write a recursive function to print first N odd natural numbers in reverse order
+5. Write a recursive function to print first N even natural numbers
+
+*/
+
+
+/*
+1. Write a recursive function to print first N natural numbers
+*/
+
+
+/*
+2. Write a recursive function to print first N natural numbers in reverse order
+*/
+
+
+/*
+3. Write a recursive function to print first N odd natural numbers
+*/
+
+
+/*
+4. Write a recursive function to print first N odd natural numbers in reverse order
+*/
+
+
+/*
+5. Write a recursive function to print first N even natural numbers
+*/
---
+++ C/Recursion/assignment_26.c
@@ -0,0 +1,34 @@
+/*
+C Language
+Assignment-26: Recursion
+1. Write a recursive function to print first N even natural numbers in reverse order
+2. Write a recursive function to print squares of first N natural numbers
+3. Write a recursive function to print binary of a given decimal number
+4. Write a recursive function to print octal of a given decimal number
+5. Write a recursive function to print reverse of a given number
+*/
+
+
+/*
+1. Write a recursive function to print first N even natural numbers in reverse order
+*/
+
+
+/*
+2. Write a recursive function to print squares of first N natural numbers
+*/
+
+
+/*
+3. Write a recursive function to print binary of a given decimal number
+*/
+
+
+/*
+4. Write a recursive function to print octal of a given decimal number
+*/
+
+
+/*
+5. Write a recursive function to print reverse of a given number
+*/

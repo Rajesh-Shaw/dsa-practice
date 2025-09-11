ac269b99cf730e3c7cf776a35068335c1bc98edd
Rajesh-Shaw <rkshaw7200@gmail.com>
2025-09-11 20:11:19 +0530

    Add Assignment 27,28

---
+++ C/Recursion/assignment_27.c
@@ -0,0 +1,43 @@
+/*
+C Language
+Assignment-27: More on Recursion
+1. Write a recursive function to calculate sum of first N natural numbers
+2. Write a recursive function to calculate sum of first N odd natural numbers
+3. Write a recursive function to calculate sum of first N even natural numbers
+4. Write a recursive function to calculate sum of squares of first n natural numbers
+5. Write a recursive function to calculate sum of digits of a given number
+*/
+
+
+
+/*
+1. Write a recursive function to calculate sum of first N natural numbers
+*/
+
+
+
+
+/*
+2. Write a recursive function to calculate sum of first N odd natural numbers
+*/
+
+
+
+
+/*
+3. Write a recursive function to calculate sum of first N even natural numbers
+*/
+
+
+
+/*
+4. Write a recursive function to calculate sum of squares of first n natural numbers
+*/
+
+
+
+/*
+5. Write a recursive function to calculate sum of digits of a given number
+*/
+
+
---
+++ C/Recursion/assignment_28.c
@@ -0,0 +1,43 @@
+/*
+C Language
+Assignment-28: More on Recursion
+1. Write a recursive function to calculate factorial of a given number
+2. Write a recursive function to calculate HCF of two numbers
+3. Write a recursive function to print first N terms of Fibonacci series
+4. Write a program in C to count the digits of a given number using recursion.
+5. Write a program in C to calculate the power of any number using recursion.
+*/
+
+
+
+/*
+1. Write a recursive function to calculate factorial of a given number
+*/
+
+
+
+
+/*
+2. Write a recursive function to calculate HCF of two numbers
+*/
+
+
+
+
+/*
+3. Write a recursive function to print first N terms of Fibonacci series
+*/
+
+
+
+/*
+4. Write a program in C to count the digits of a given number using recursion.
+*/
+
+
+
+/*
+5. Write a program in C to calculate the power of any number using recursion.
+*/
+
+

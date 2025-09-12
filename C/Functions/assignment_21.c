7eb786dc3bdba14cc6e911f9728f839525e48d21
Rajesh-Shaw <rkshaw7200@gmail.com>
2025-09-12 11:24:30 +0530

    Update assignment_21 and assignment_22 with latest changes

--- C/Functions/assignment_21.c
+++ C/Functions/assignment_21.c
@@ -8,23 +8,104 @@ Assignment-21: Functions
 5. Write a function to print first N odd natural numbers. (TSRN)
 */
 
+#include<stdio.h>
 /*
 1. Write a function to calculate the area of a circle. (TSRS)
 */
+ 
+float circleArea(float r)
+{
+    return 3.14*r*r;
+}
 
 /*
 2. Write a function to calculate simple interest. (TSRS)
 */
 
+float simple_interest(float p, float r, float t)
+{
+    return (p*r*t)/100;
+}
+
 /*
 3. Write a function to check whether a given number is even or odd. Return 1 if the number is even, otherwise return 0. (TSRS)
 */
+
+int isEven(int n)
+{
+    if(n%2==0)
+        return 1;
+    else
+        return 0;
+    
+    // return (n % 2 == 0);
+}
 
 /*
 4. Write a function to print first N natural numbers (TSRN)
 */
 
+void natural_numbers(int n)
+{
+    for(int i=1; i<=n; i++)
+        printf("%d\n",i);
+}
+
 
 /*
 5. Write a function to print first N odd natural numbers. (TSRN)
 */
+void odd_natural_numbers(int n)
+{
+    for(int i=1; i<=n; i++)
+        printf("%d\n",2*i-1);
+}
+
+
+
+int main()
+{
+    // // Q1
+    // float r=0,area;
+    // printf("Enter circle Redius :");
+    // scanf("%f",&r);
+    // area=circleArea(r);
+    // printf("Area of Circle %.2f",area);
+
+
+    // // Q2
+    // float p,r,t,si;
+    // printf("Enter Principle, rate of interest and time in year :");
+    // scanf("%f%f%f",&p,&r,&t);
+    // si=simple_interest(p,r,t);
+    // printf("Simple Interest %.2f",si);
+
+
+    // // Q3
+    // int n=0,result=0;
+    // printf("Enter a number :");
+    // scanf("%d",&n);
+    // result=isEven(n);
+    // if(result)
+    //     printf("%d is an Even Number.",n);
+    // else
+    //     printf("%d is an Odd Number.",n);
+
+
+    // // Q4
+    // int n=0;
+    // printf("Enter a number :");
+    // scanf("%d",&n);
+    // natural_numbers(n);
+
+
+
+    // Q5
+    int n=0;
+    printf("Enter a number :");
+    scanf("%d",&n);
+    odd_natural_numbers(n);
+   
+
+    return 0;
+}
--- C/Functions/assignment_22.c
+++ C/Functions/assignment_22.c
@@ -4,20 +4,54 @@ Assignment-22: Functions
 1. Write a function to calculate the factorial of a number. (TSRS)
 2. Write a function to calculate the number of combinations one can make from n items and r selected at a time. (TSRS)
 3. Write a function to calculate the number of arrangements one can make from n items and r selected at a time. (TSRS)
-4. Write a function to check whether a given number contains a given digit or not.
-(TSRS)
+4. Write a function to check whether a given number contains a given digit or not.(TSRS)
 5. Write a function to print all prime factors of a given number. For example, if the number is 36 then your result should be 2, 2, 3, 3. (TSRN)
 */
 
+
+#include<stdio.h>
+unsigned long long factorial(int n);
+unsigned long long combinations(int n, int r);
+unsigned long long arrangements(int n, int r);
+int isDigitContain(int n, int d);
+void all_prime_factors(int n);
+
+int main()
+{
+    return 0;
+}
 /*
 1. Write a function to calculate the factorial of a number. (TSRS)
 */
+unsigned long long factorial(int n)
+{
+    if (n < 0)
+    {
+        printf("Error: Factorial of a negative number is not defined.\n");
+        return 0;
+    }
+
+    unsigned long long f = 1;
+    for(int i=1; i<=n; i++)
+        f=f*i;
+    
+    return f;
+}
 
 
 /*
 2. Write a function to calculate the number of combinations one can make from n items and r selected at a time. (TSRS)
 
 */
+unsigned long long combinations(int n, int r)
+{
+    if (r > n || n < 0 || r < 0) 
+    {
+        printf("Invalid input: n must be >= r and both non-negative.\n");
+        return 0;
+    }
+    return factorial(n)/(factorial(r)*factorial(n-r));
+}
 
 
 /*
@@ -25,14 +59,61 @@ Assignment-22: Functions
 
 */
 
+unsigned long long arrangements(int n, int r)
+{
+    if (r > n || n < 0 || r < 0) 
+    {
+        printf("Invalid input: n must be >= r and both non-negative.\n");
+        return 0;
+    }
+    return factorial(n)/factorial(n-r);
+}
 
 /*
-4. Write a function to check whether a given number contains a given digit or not.
-(TSRS)
+4. Write a function to check whether a given number contains a given digit or not.(TSRS)
 */
 
+int isDigitContain(int n, int d)
+{
+    n = n < 0 ? -n : n;
+    while(n!=0)
+    {
+        if(d==(n%10))
+            return 1;
+        n=n/10;
+    }
+    return 0;
+}
 
 /*
 5. Write a function to print all prime factors of a given number. For example, if the number is 36 then your result should be 2, 2, 3, 3. (TSRN)
-
 */
+
+void all_prime_factors(int n)
+{
+    for(int i=2;i<=n;i++)
+    {
+        while(n%i==0)
+        {
+            printf("%d ", i);
+            n = n / i;
+        }
+    }
+}
+
+/*
+void all_prime_factors(int n)
+{
+    for(int i=2; i*i <= n; i++)
+    {
+        while(n % i == 0)
+        {
+            printf("%d ", i);
+            n = n / i;
+        }
+    }
+    if(n > 1)  
+        printf("%d ", n);
+}
+*/
+

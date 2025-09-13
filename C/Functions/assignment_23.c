c1fb17932cc3d368c49a91e9ca29a67b290d902a
Rajesh-Shaw <rkshaw7200@gmail.com>
2025-09-13 09:30:19 +0530

    Update Assignment 23,24

--- C/Functions/assignment_23.c
+++ C/Functions/assignment_23.c
@@ -8,23 +8,116 @@ Assignment-23: More on Functions
 5. Write a function to print first N prime numbers (TSRN)
 */
 
+
+#include<stdio.h>
+int lcm(int a, int b);
+int hcf(int a, int b);
+int isPrime(int n);
+int nextPrime(int n);
+void firstN_Prime(int n);
+
+int main()
+{
+    return 0;
+}
+
 /*
 1. Write a function to calculate LCM of two numbers. (TSRS)
+
+int lcm(int a, int b)
+{
+    int max=0, i=0;
+    max = a>b?a:b;
+    for(i=max; i<=a*b; i++)
+    {
+        if(i%a==0 && i%b==0)
+            return i;
+    }
+    return a*b;
+}
 */
+int lcm(int a, int b)
+{
+    if(a == 0 || b == 0) 
+        return 0;
+
+    return (a*b)/hcf(a,b);
+}
 
 /*
 2. Write a function to calculate HCF of two numbers. (TSRS)
+
+int hcf(int a, int b)
+{
+    int min = (a < b) ? a : b;
+    for(int i = min; i >= 1; i--)
+    {
+        if(a%i==0 && b%i==0)
+            return i;
+    }
+    return 1;
+}
+
 */
+
+int hcf(int a, int b)
+{
+    while(b != 0)
+    {
+        int temp = b;
+        b = a%b;
+        a = temp;
+    }
+    return a;
+}
+
+
 
 /*
 3. Write a function to check whether a given number is Prime or not. (TSRS)
 */
+int isPrime(int n)
+{
+    if (n <= 1) 
+        return 0; 
 
+    for(int i=2; i*i<=n; i++)
+    {
+        if(n%i==0)
+            return 0;
+    }
+    return 1;
+}
 
 /*
 4. Write a function to find the next prime number of a given number. (TSRS)
 */
+int nextPrime(int n)
+{
+    int next_prime=0;
+    for(n=n+1; ; n++)
+    {
+        next_prime = isPrime(n);
+        if(next_prime)
+            return n;
+    }
+    return -1;
+}
 
 /*
 5. Write a function to print first N prime numbers (TSRN)
 */
+void firstN_Prime(int n)
+{
+    int count = 0, num = 2;
+    while(count < n)
+    {
+        if(isPrime(num))
+        {
+            printf("%d ", num);
+            count++;
+        }
+        num++;
+    }
+
+}
--- C/Functions/assignment_24.c
+++ C/Functions/assignment_24.c
@@ -6,30 +6,189 @@ Assignment-24: More on Functions
 3. Write a function to print PASCAL Triangle. (TSRN)
 4. Write a function to print all Armstrong numbers between two given numbers.
 (TSRN)
-5. Write a function to find the sum of first N terms of the series 11/1+2!/2+3!/3+4!/4+5!/
-5... n!/n (TSRS)
+5. Write a function to find the sum of first N terms of the series 1!/1 + 2!/2 + 3!/3 + 4!/4 + 5!/5... n!/n  (TSRS)
 
 */
+
+
+
+#include<stdio.h>
+int isPrime(int n);
+void allPrime(int a, int b);
+
+void fibonacci(int n);
+void pascalTriangle(int n);
+long factorial(int n);
+long nCr(int n, int r);
+
+void allArmstrong(int a, int b);
+int armstrong(int n);
+long sum_of_n_series(int n);
+
+int main()
+{
+    int n;
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+    pascalTriangle(n);
+
+    return 0;
+}
 
 /*
 1. Write a function to print all Prime numbers between two given numbers. (TSRN)
 */
 
+void allPrime(int a, int b)
+{
+    for(a; a<=b; a++)
+    {
+        if(isPrime(a))
+            printf("%d ",a);
+    }
+}
+
+int isPrime(int n)
+{
+    if(n<=1)
+        return 0;
+    for(int i=2; i*i<=n; i++)
+    {
+        if(n%i==0)
+            return 0;
+    }
+    return 1;
+}
+
+
 /*
 2. Write a function to print first N terms of Fibonacci series (TSRN)
 */
+void fibonacci(int n)
+{
+    int a=0, b=1, c=1;
+    if(n==1)
+        {
+            printf("Fibonacci series :\n %d ",a);
+            return;
+        }
+    if(n==2)
+        {printf("Fibonacci series :\n %d %d ",a,b);
+            return;
+        }
+
+    for(int i=3; i<=n; i++)
+    {
+        c=a+b;
+        a=b;
+        b=c;
+        printf("%d ",c);
+    }
+}
+
+
 
 /*
 3. Write a function to print PASCAL Triangle. (TSRN)
+
+     :1 2 3 4 5 6 ...=(2*n-1)      
+
+Row 1:          1
+Row 2:        1   1
+Row 3:      1   2   1
+Row 4:    1   3   3   1
+Row 5:  1   4   6   4   1
+Row 6:1   5  10  10   5   1
+
+i   j                   J
+1   6                   
+2   5,7         
+3   4,6,8
+4   3,5,7,9             
+5   2,4,6,8,10
+6   1,3,5,7,9,11
+
 */
+void pascalTriangle(int rows)
+{
+    for(int i=1; i<=rows; i++)
+    {
+        for (int space=1; space<=rows-i; space++)
+            printf("  ");
+
+        for (int j = 0; j < i; j++)
+            printf("%4ld ", nCr(i-1, j));
+        
+        printf("\n");
+    }
+}
+
+// Function to calculate factorial
+long factorial(int n) 
+{
+    long f = 1;
+    for (int i = 1; i <= n; i++)
+        f *= i;
+    return f;
+}
+
+// Function to calculate nCr
+long nCr(int n, int r) 
+{
+    return factorial(n) / (factorial(r) * factorial(n - r));
+}
 
 
 /*
 4. Write a function to print all Armstrong numbers between two given numbers.
 (TSRN)
 */
+void allArmstrong(int a, int b)
+{
+    for(a; a<=b; a++)
+    {
+        if(armstrong(a))
+        {
+            printf("%d ",a);
+        }
+    }
 
+}
+
+
+int armstrong(int n)
+{
+    int c=0,r=0,num=n,arm=0,powr=1;
+    while(num!=0)
+    {
+        num=num/10;
+        c++;
+    }
+    num=n;
+    while(num!=0)
+    {
+        r=num%10;
+        powr = 1;
+        for(int i=1; i<=c; i++)
+            powr =powr*r;
+        arm=arm+powr;
+        num=num/10;
+    }
+    if(n==arm)
+        return 1;
+    else
+        return 0;        
+}
 
 /*
-5. Write a function to find the sum of first N terms of the series 11/1+2!/2+3!/3+4!/4+5!/5... n!/n (TSRS)
+5. Write a function to find the sum of first N terms of the series 1!/1 + 2!/2 + 3!/3 + 4!/4 + 5!/5... n!/n (TSRS)
 */
+long sum_of_n_series(int n)
+{
+    long sum=0;
+    for(int i=1; i<=n; i++)
+    {
+        sum=sum + factorial(i)/i;
+    }
+    return sum;
+}

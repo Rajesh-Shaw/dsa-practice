704f3b36f940c9334b3047381c8ee8bd7b6957fa
Rajesh-Shaw <rkshaw7200@gmail.com>
2025-10-08 13:30:39 +0530

    Update assignment 32

--- C/Arrays/assignment_32.c
+++ C/Arrays/assignment_32.c
@@ -11,10 +11,10 @@ Assignment-32: Arrays and Functions
 
 #include<stdio.h>
 void swap_two_elements_in_array(int a[], int n, int index1, int index2);
-void total_duplicate_elements();
-void print_unique_element();
-void merge_array();
-void count_frequency_element_of_array();
+void total_duplicate_elements(int a[], int n);
+void print_unique_element(int a[], int n);
+void merge_array(int a[], int n);
+void count_frequency_element_of_array(int a[], int n);
 int main()
 {
     int n, index1, index2;
@@ -26,10 +26,12 @@ int main()
     for(int i=0; i<n; i++)
         scanf("%d",&a[i]);
 
-    printf("Enter Two indices for swap values of array : ");
-    scanf("%d%d",&index1, &index2);
+    // printf("Enter Two indices for swap values of array : ");
+    // scanf("%d%d",&index1, &index2);
 
-    swap_two_elements_in_array(a, n, index1, index2 );
+    // swap_two_elements_in_array(a, n, index1, index2 );
+
+    total_duplicate_elements(a, n);
 
     return 0;
 }
@@ -61,7 +63,34 @@ void swap_two_elements_in_array(int a[], int n, int index1, int index2)
 2. Write a function to count a total number of duplicate elements in an array. (Means elements that occurs 2 times in an array)
 
 */
+void total_duplicate_elements(int a[], int n)
+{
+    int c=0;
+    int visited[n];
+    for(int i=0; i<n; i++)
+        visited[i] = 0;
 
+    for(int i=0; i<n; i++)
+    {
+        if(visited[i]) 
+            continue;
+
+        int count = 1;
+        for(int j=i+1; j<n; j++)
+        {
+            if(a[i]==a[j])
+            {
+                count++;
+                visited[j] = 1;
+            }
+        }
+
+        if(count > 1) 
+            c++;
+
+    }
+    printf("Total number of duplicate elements in an array %d",c);
+}
 
 
 /*
